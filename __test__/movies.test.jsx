73c7d043bca025a468859e84b37e8962181beac4
Kaiahe <kahe009@student.kristiania.no>
2022-01-28 12:56:40 +0100

    Simulate test, onAddMovie not a function

--- __test__/movies.test.jsx
+++ __test__/movies.test.jsx
@@ -1,22 +1,7 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
-import {ListMovies} from "../application";
-
-function NewMovieForm() {
-    return <form>
-        <h1>Add new movie</h1>
-        <div>
-            Title: <input/>
-        </div>
-        <div>
-            Year: <input/>
-        </div>
-        <div>
-            Plot:
-            <div><textarea/></div>
-        </div>
-    </form>
-}
+import {ListMovies, NewMovieForm} from "../application";
+import {Simulate} from "react-dom/test-utils";
 
 describe("movies application", () => {
 
@@ -32,5 +17,28 @@ describe("movies application", () => {
         const element = document.createElement("div");
         ReactDOM.render(<NewMovieForm/>, element);
         expect(element.innerHTML).toMatchSnapshot();
+    });
+
+    it("submit new movie", () => {
+        const onAddMovie = jest.fn();
+
+        const element = document.createElement("div");
+        ReactDOM.render(<NewMovieForm onAddMovie={onAddMovie} />, element);
+
+        Simulate.input(
+            //Finn elementet med data-testid=title og send en input event til det
+            element.querySelector("[data-testid=title]"),
+                {target:{value: "Movie 1"}}
+        );
+        Simulate.input(
+            //Finn elementet med data-testid=year og send en input event til det
+            element.querySelector("[data-testid=year]"), {target:{value: "2022"}}
+        );
+        //Finn elementet av type form og send en submit event til det
+        Simulate.submit(element.querySelector("form"));
+
+        expect(onAddMovie).toHaveBeenCalledWith( {
+            title: "Movie 1", year: "2022", plot: ""
+        })
     })
 })
--- application.jsx
+++ application.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import {useState} from "react";
 
 export function ListMovies() {
     return <div>
@@ -8,4 +9,28 @@ export function ListMovies() {
             <li>Movie 2</li>
         </ul>
     </div>
+}
+
+export function NewMovieForm(onAddMovie) {
+    const [title, setTitle] = useState("");
+    const [year, setYear] = useState("");
+    const [plot, setPlot] = useState("");
+
+    function handleSubmit() {
+        onAddMovie({title, year, plot});
+    }
+
+    return <form onSubmit={handleSubmit}>
+        <h1>Add new movie</h1>
+        <div>
+            Title: <input data-testid="title" value={title} onChange={e => setTitle(e.target.value)}/>
+        </div>
+        <div>
+            Year: <input data-testid="year" value={year} onChange={e => setYear(e.target.value)}/>
+        </div>
+        <div>
+            Plot:
+            <div><textarea data-testid="plot" value={plot} onChange={e => setPlot(e.target.value)}/></div>
+        </div>
+    </form>
 }
