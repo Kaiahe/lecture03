e21f413468b2393681badd0e8301fafd6f2f5be3
Kaiahe <kahe009@student.kristiania.no>
2022-01-28 12:00:26 +0100

    Add snapshot test for <NewMovieForm/>

--- __test__/movies.test.jsx
+++ __test__/movies.test.jsx
@@ -2,6 +2,22 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import {ListMovies} from "../application";
 
+function NewMovieForm() {
+    return <form>
+        <h1>Add new movie</h1>
+        <div>
+            Title: <input/>
+        </div>
+        <div>
+            Year: <input/>
+        </div>
+        <div>
+            Plot:
+            <div><textarea/></div>
+        </div>
+    </form>
+}
+
 describe("movies application", () => {
 
     it("shows movie list", () => {
@@ -9,6 +25,12 @@ describe("movies application", () => {
         ReactDOM.render(<ListMovies/>, element);
         expect(element.querySelector("h1").innerHTML)
             .toEqual("List movies");
+        expect(element.innerHTML).toMatchSnapshot();
+    });
+
+    it("shows new movie form", () => {
+        const element = document.createElement("div");
+        ReactDOM.render(<NewMovieForm/>, element);
         expect(element.innerHTML).toMatchSnapshot();
     })
 })
